5464f6e6217322bc7edcc589de5524bcc53fd152
Patrick Kelly <patrick.kelly@wisconsin.gov>
2026-03-26 15:19:48 -0500

    updated length plot feature

--- app.R
+++ app.R
@@ -58,6 +58,37 @@ colnames(sum.latlon_df) <- c('lng', 'lat')
 #combine with water quality data
 sum.all <- cbind(sum.all, sum.latlon_df)
 
+#Now do the same with lengths data
+length_url <- 'https://raw.githubusercontent.com/patrickDNR/Pool-8-Fish_CPUE-mapping/refs/heads/main/Data/Lengths_allgear.csv'
+download.file(length_url, 'Lengths_allGear.csv')
+
+length.all <- read.csv('Lengths_allGear.csv') %>%
+  dplyr::mutate(date = as.Date(format(mdy(sdate), '%Y-%m-%d'))) %>%
+  dplyr::mutate(Month = month(mdy(sdate), label = T)) %>%
+  select(code,Fishname,sdate, X0., X25., X50., X75., X100., date, Month, gear,zone15e, zone15n, temp, do, vegd, stratum, year) %>%
+  rename('0%' = 'X0.', '25%' = 'X25.', '50%' = 'X50.', 
+         '75%' = 'X75.', '100%' = 'X100.')
+
+#Fix the location data just like with cpue
+#Convert to sf object
+length.utm_sf <- st_as_sf(length.all, coords = c('zone15e', 'zone15n'), crs = utm_crs)
+
+#transform to geographic coordinates
+length.latlon_sf <- st_transform(length.utm_sf, crs = 4326)
+
+#make into data frame
+length.latlon_df <- as.data.frame(st_coordinates(length.latlon_sf))
+colnames(length.latlon_df) <- c('lng', 'lat')
+
+#combine with water quality data
+length.all <- cbind(length.all, length.latlon_df)
+
+#change the direction of the data...
+length.all <- pivot_longer(length.all, 
+                     cols = c('0%', '25%', '50%', '75%', '100%'), 
+                     names_to = 'Metric', 
+                     values_to = 'Measurement')
+
 # Define UI for water quality map app ----
 ui <- 
   
@@ -88,14 +119,14 @@ ui <-
     #Now for the good stuff, make a sidebar first...
     
     nav_panel(
-      title = "CPUE Time series plot",
+      title = "CPUE Time series",
       # Sidebar layout with input and output definitions ----
       sidebarLayout(
         # Sidebar panel for inputs ----
         sidebarPanel(
           
           # Input: Maybe a slider for time series?
-          sliderInput(inputId = 'date_range', 
+          sliderInput(inputId = 'CPUEts_date_range', 
                       label = 'Select Date Range:', 
                       min = min(cpue$Year), 
                       max = max(cpue$Year),
@@ -105,7 +136,7 @@ ui <-
                       ticks = F),
           
           #Select months of interest
-          checkboxGroupInput(inputId = 'months', 
+          checkboxGroupInput(inputId = 'CPUEts_months', 
                              label = 'Select Sampling Months:', 
                              choices = c('June' = 'Jun', 
                                          'July' = 'Jul', 
@@ -117,14 +148,14 @@ ui <-
                                           'Oct', 'Nov')),
           
           # Input: Enter gear type
-          selectInput(inputId = 'gear_type', 
+          selectInput(inputId = 'CPUEts_gear_type', 
                       label = 'Select Gear Type:', 
                       choices = c('Large Fyke' = 'F', 
                                   'Mini Fyke' = 'M', 
                                   'Day electroshocking' = 'D')),
           
           #Input: Select constituent
-          selectInput(inputId = 'fish', 
+          selectInput(inputId = 'CPUEts_fish', 
                       label = 'Select Species:', 
                       choices = c('Bigmouth buffalo', 
                                   'Black bullhead', 'Black crappie', 
@@ -144,7 +175,7 @@ ui <-
                                   'Yellow bullhead', 'Yellow perch')),
           
           #Add input for habitat
-          checkboxGroupInput(inputId = 'habitat', 
+          checkboxGroupInput(inputId = 'CPUEts_habitat', 
                              label = 'Select Stratum:', 
                              choices = c('Wing Dam' = 'MCB-W', 
                                          'Main Channel Border - Unstructured' = 'MCB-U', 
@@ -157,7 +188,7 @@ ui <-
                                           'SCB', 'TWZ')),
           
           #checkbox to select if you want to show outliers or not
-          checkboxInput(inputId = 'outliers', 
+          checkboxInput(inputId = 'CPUEts_outliers', 
                         label = 'Show outliers:', 
                         value = TRUE),
           
@@ -260,7 +291,7 @@ ui <-
     
     #Page 3 -- plot diversity data...
     nav_panel(
-      title = 'Diversity time series plot',
+      title = 'Diversity time series',
       
       sidebarLayout(
         sidebarPanel(
@@ -320,6 +351,93 @@ ui <-
           plotOutput('divBoxes', , height = 600, width = 1000)
         )
       )
+    ), 
+    
+    #Page 4....make a plot of fish lengths
+    nav_panel(
+      title = "Median fish lengths time series",
+      # Sidebar layout with input and output definitions ----
+      sidebarLayout(
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+          
+          # Input: Maybe a slider for time series?
+          sliderInput(inputId = 'length_date_range', 
+                      label = 'Select Date Range:', 
+                      min = min(length.all$year), 
+                      max = max(length.all$year),
+                      step = 1,
+                      value = c(min(length.all$year), max(length.all$year)), 
+                      sep = '', 
+                      ticks = F),
+          
+          #Select months of interest
+          checkboxGroupInput(inputId = 'length_months', 
+                             label = 'Select Sampling Months:', 
+                             choices = c('June' = 'Jun', 
+                                         'July' = 'Jul', 
+                                         'August' = 'Aug', 
+                                         'September' = "Sep", 
+                                         'October' = 'Oct', 
+                                         'November' = 'Nov'), 
+                             selected = c('Jun', 'Jul', 'Aug', 'Sep', 
+                                          'Oct', 'Nov')),
+          
+          # Input: Enter gear type
+          selectInput(inputId = 'length_gear_type', 
+                      label = 'Select Gear Type:', 
+                      choices = c('Large Fyke' = 'F', 
+                                  'Mini Fyke' = 'M', 
+                                  'Day electroshocking' = 'D')),
+          
+          #Input: Select constituent
+          selectInput(inputId = 'length_fish', 
+                      label = 'Select Species:', 
+                      choices = c('Bigmouth buffalo', 
+                                  'Black bullhead', 'Black crappie', 
+                                  'Bluegill', 'Bowfin', 'Brown bullhead', 
+                                  'Burbot', 'Central mudminnow', 'Channel catfish', 'Common carp', 
+                                  'Emerald shiner', 'Fathead minnow', 'Freshwater drum', 
+                                  'Gizzard shad', 'Golden redhorse', 'Golden shiner', 'Green sunfish', 
+                                  'Largemouth bass', 'Longnose gar', 'Mimic shiner', 'Mooneye', 
+                                  'Northern pike', 'Orangespotted sunfish', 'Pugnose minnow', 
+                                  'Pumpkinseed', 'Quillback', 'River carpsucker', 'River darter', 
+                                  'River shiner', 'Rock bass', 'Sand shiner', 'Sauger', 'Shorthead redhorse', 
+                                  'Shortnose gar', 'Shovelnose sturgeon', 'Silver chub', 'Silver redhorse', 
+                                  'Smallmouth bass', 'Smallmouth buffalo', 'Spotfin shiner', 
+                                  'Spottail shiner', 'Spotted sucker', 'Stonecat', 'Tadpole madtom', 
+                                  'Walleye', 'Warmouth', 'Weed shiner', 'Western sand darter', 
+                                  'White bass', 'White crappie', 'White sucker', 'Yellow bass', 
+                                  'Yellow bullhead', 'Yellow perch')),
+          
+          #Add input for habitat
+          checkboxGroupInput(inputId = 'length_habitat', 
+                             label = 'Select Stratum:', 
+                             choices = c('Wing Dam' = 'MCB-W', 
+                                         'Main Channel Border - Unstructured' = 'MCB-U', 
+                                         'Backwater' = 'BWC-S', 
+                                         'Impounded - Shoreline' = 'IMP-S', 
+                                         'Impounded-Offshore' = 'IMP-O', 
+                                         'Side Channel' = 'SCB', 
+                                         'Tailwater'  = 'TWZ'), 
+                             selected = c('MCB-W', 'MCB-U', 'BWC-S', 'IMP-S', 'IMP-O', 
+                                          'SCB', 'TWZ')),
+          
+          #checkbox to select if you want to show outliers or not
+          checkboxInput(inputId = 'length_outliers', 
+                        label = 'Show outliers:', 
+                        value = TRUE),
+          
+          #Add the option to download the selected dataset
+          downloadButton(outputId = 'length_downloadData', 
+                         label = 'Download CSV')
+          
+        )
+        ,
+        mainPanel(
+          plotOutput('lengthPlot', height = 600, width = 1000)
+        )
+      )
     )
   )
 
@@ -347,6 +465,17 @@ server <- function(input, output) {
     
   })
   
+  filtered_data_CPUEts <- reactive({
+    cpue %>%
+      filter(Year >= input$CPUEts_date_range[1] & Year <= input$CPUEts_date_range[2]) %>%
+      filter(gear == input$CPUEts_gear_type) %>%
+      filter(Fishname == input$CPUEts_fish) %>%
+      filter(!is.na(CPUE)) %>%
+      filter(stratum %in% input$CPUEts_habitat) %>%
+      filter(Month %in% input$CPUEts_months)
+    
+  })
+  
   #filter summary data based on input parameters
   filtered_summary <- reactive({
     sum.all %>%
@@ -355,6 +484,19 @@ server <- function(input, output) {
       filter(!is.na(shannon)) %>%
       filter(stratum %in% input$sum_habitat) %>%
       filter(Month %in% input$sum_months)
+    
+  })
+  
+  #filter length data based on input parameters
+  filtered_length <- reactive({
+    length.all %>%
+      filter(year >= input$length_date_range[1] & year <= input$length_date_range[2]) %>%
+      filter(gear == input$length_gear_type) %>%
+      filter(Fishname == input$length_fish) %>%
+      filter(stratum %in% input$length_habitat) %>%
+      filter(Month %in% input$length_months) %>%
+      group_by(year, Metric) %>%
+      summarise(mean.measure = mean(Measurement, na.rm = T))
     
   })
   
@@ -371,7 +513,7 @@ server <- function(input, output) {
   
   #Generate a boxplot across habitat classes
   output$fishBoxes <- renderPlot({
-    df <- filtered_data()
+    df <- filtered_data_CPUEts()
     
     validate(
       need(nrow(df) > 0, 'No data available to display. Please adjust stratum or gear filters.')
@@ -381,7 +523,7 @@ server <- function(input, output) {
       as.numeric(df$CPUE) ~ as.numeric(df$Year), 
       xlab = 'Year',
       ylab = paste(input$fish, unique(df$lab)), 
-      outline = input$outliers, 
+      outline = input$CPUEts_outliers, 
       cex.lab = 1.5, 
       cex.axis = 1.5
     )
@@ -438,7 +580,7 @@ server <- function(input, output) {
     df <- filtered_summary()
     
     validate(
-      need(nrow(df) > 0, 'No data available to display. Please adjust stratum or gear filters.')
+      need(nrow(df) > 0,'No data available to display. Please adjust stratum or gear filters.')
     )
     
     boxplot(
@@ -458,6 +600,34 @@ server <- function(input, output) {
     },
     content = function(file){
       write.csv(filtered_summary(), file, row.names = FALSE)
+    }
+  )
+  
+  output$lengthPlot <- renderPlot({
+    df.length <- filtered_length()
+    
+    validate(
+      need(nrow(df.length) > 0,'No data available to display. Please adjust stratum or gear filters.')
+    )
+    
+    ggplot(data = df.length[df.length$Metric == '25%' |
+                              df.length$Metric == '50%' |
+                              df.length$Metric == '75%',], aes(x = year, y = mean.measure, color = Metric)) + 
+      geom_point(size = 6) + 
+      scale_color_manual('Quantile', values = c('25%' = 'darkblue', '50%' = 'black', '75%' = 'red')) +
+      theme_bw() + 
+      theme(text = element_text(size = 25))+
+      labs(x = 'Year', 
+           y = 'mean Length (mm)')
+    
+  })
+  
+  output$length_downloadData <- downloadHandler(
+    filename = function(){
+      paste('FISH_Lengthdata-', Sys.Date(), '.csv', sep = '')
+    },
+    content = function(file){
+      write.csv(filtered_length(), file, row.names = FALSE)
     }
   )
   
